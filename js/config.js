88a00d0971b9f433ebd71641358469d68bdcbc0a
ukmdavis <40501245+ukmdavis@users.noreply.github.com>
2026-08-10 21:13:33 +0100

    Update Supabase URL and ANON key in config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Paste your own Supabase project values here.
 // Find them in Supabase: Project Settings > API
-const SUPABASE_URL = "https://xhedxbccytvprkxkdtbk.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhoZWR4YmNjeXR2cHJreGtkdGJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyMDQxNDMsImV4cCI6MjEwMTc4MDE0M30.zj9g40adLnFm5P_6OWLN3xJY4c1dtiTJLyi714QDlQs;
+const SUPABASE_URL = "https://xhedxbccytvprkxkdtbk.supabase.co;
+const SUPABASE_ANON_KEY = "sb_publishable_cNmNIiemcKalHuZcLclupw_W_pEFnGZ";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
